8a2ff34ce04144aba8d955b0ad99bd737cbbd51d
Erik Huelsmann <ehuels@gmail.com>
2018-04-14 07:35:56 +0000

    * Add the text of the release e-mail to the mail script

--- notify/notification-helpers/release-email.sh
+++ notify/notification-helpers/release-email.sh
@@ -28,7 +28,28 @@ createEmail() {
 	From: ${cfgValue[mail_FromAddress]}
 	Subject: LedgerSMB $release_version released
 
+	The LedgerSMB development team is happy to announce yet another new
+	version of its open source ERP and accounting application.
+	This release contains the following fixes and improvements:
+
 	$release_changelog
+
+
+	For installation instructions and system requirements, see
+	   https://github.com/ledgersmb/LedgerSMB/blob/$release_version/README.md
+
+	The release can be downloaded from our download site at
+	   https://download.ledgersmb.org/f/Releases/$release_version
+
+	The release can be downloaded from GitHub at
+	   https://github.com/ledgersmb/LedgerSMB/releases/tag/$release_version
+
+	Or pulled from Docker Hub using the command
+	   $ docker pull ledgersmb/ledgersmb:$release_version
+
+	These are the sha256 checksums of the uploaded files:
+
+	$release_sha256sums
 
 EOF
     $Editor /tmp/msg.txt
